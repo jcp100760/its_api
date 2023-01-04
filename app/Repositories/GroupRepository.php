00a3fdabc6e7d6a88d40232fd76317c1952f5b08
jcp1007 <julioparis100760@gmail.com>
2023-01-03 21:10:47 -0300

    actualizacion 03012023_2110

--- app/Repositories/GroupRepository.php
+++ app/Repositories/GroupRepository.php
@@ -1,31 +1,56 @@
 <?php
 
-namespace App\Repositories\Matter;
+namespace App\Repositories;
 
-use App\Models\Matter;
-use App\Http\Resources\MatterResource;
+use App\Models\Group;
 use App\Repositories\BaseRepository;
 
-class MatterRepository extends BaseRepository
+class GroupRepository extends BaseRepository
 {
     public function getModel()
     {
-        return new Matter();
+        return new Group();
     }
 
-    public function getMatterByDescription($description)
+    public function getAllByProffessors()                                       //probado funciona
     {
-        $matterDescription = $this->model->where('description', 'like',  '%' . $description. '%' )->get();
-        return MatterResource::collection($matterDescription);
+        return $this->getModel()->with(['proffessors'])->get();
     }
 
-    public function getMatterByNameDescription($search)
+    public function getGroupByDescription($description)
     {
-        $matterNameDescription = $this->model
+        return $this->getModel()->where('description', 'like',  '%' . $description. '%' )->get();
+    }
+
+    public function getGroupByNameDescription($search)
+    {
+        $groupNameDescription = $this->getModel()
             ->where('name', 'like',  '%' . $search . '%')
             ->orWhere('description', 'like',  '%' . $search . '%')
-            ->orderBy('name', 'asc');
-        return MatterResource::collection($matterNameDescription);
+            ->orderBy('name', 'asc')->get();
+        return $groupNameDescription;
+    }
+
+    public function matterProffessorRelation($id, $request)
+    {
+        $proffessorId = $request->proffessorId; 
+        $matter = $this->getModel()->find($id);
+        $matter->proffessors()->attach($proffessorId);
+        return response()->json([
+                'message' => 'Registro relacionado exitosamente', 
+                'code' => 200
+            ]); 
+    }
+
+    public function matterDetachRelation($id, $request)
+    {
+        $proffessorId = $request->proffessorId; 
+         $matter = $this->getModel()->find($id);
+        $matter->proffessors()->detach($proffessorId);
+        return response()->json([
+                'message' => 'Relacion borrada exitosamente', 
+                'code' => 200
+            ]); 
     }
 
 }
